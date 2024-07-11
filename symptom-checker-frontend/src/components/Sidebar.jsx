866050cbcfe1713cd900d1c142afa17ece3bfad3
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-11 16:35:44 +0300

    chore: Update styles and links

--- symptom-checker-frontend/src/components/Sidebar.jsx
+++ symptom-checker-frontend/src/components/Sidebar.jsx
@@ -12,39 +12,68 @@ export default function Sidebar({ open, toggleDrawer }){
         open: PropTypes.bool.isRequired,
         toggleDrawer: PropTypes.func.isRequired,
     }
+
+    const sidebarStyles = {
+        drawer: { 
+            width: 300, 
+            flexShrink: 0 
+        },
+        list: {
+            width: '100%', 
+            maxWidth: 360, 
+            bgcolor: 'background.paper', 
+            display: 'flex', 
+            flexDirection: 'column', 
+            justifyContent: 'space-between' 
+        },
+        logo: { 
+            justifyContent: 'center', 
+            display: 'flex', 
+            my: 10 
+        },
+        listItemButton:{ 
+            pl: 4 
+        },
+        link: { 
+            textDecoration: 'none', 
+            color: 'inherit' 
+        }
+    }
     
     return(
-        <Drawer open={open} onClose={toggleDrawer} >
-            <List>
-                <ListItemIcon>
-                    <img src={Logo} alt='Logo' width={60} height={60} style={{padding: 10, margin: 10}} />
+        <Drawer open={open} onClose={toggleDrawer} sx={sidebarStyles.drawer} >
+            <List sx={sidebarStyles.list}>
+                <ListItemIcon sx={sidebarStyles.logo}>
+                    <img src={Logo} alt='Logo' 
+                    width={60} height={60} 
+                    />
                 </ListItemIcon>
-                <Link to="/home">
-                    <ListItemButton>
+                <Link to="/" style={sidebarStyles.link}>
+                    <ListItemButton sx={sidebarStyles.listItemButton}>
                         <ListItemIcon>
                             <HomeOutlined />
                         </ListItemIcon>
                         <ListItemText primary="Home" />
                     </ListItemButton>
                 </Link>
-                <Link to="/profile">
-                    <ListItemButton>
-                        <ListItemIcon>
+                <Link to="/profile" style={sidebarStyles.link}>
+                <ListItemButton sx={sidebarStyles.listItemButton}>
+                    <ListItemIcon>
                             <Person />
                         </ListItemIcon>
                         <ListItemText primary="Profile" />
                     </ListItemButton>
                 </Link>
-                <Link to="/faqs">
-                    <ListItemButton>
+                <Link to="/faqs" style={sidebarStyles.link}>
+                    <ListItemButton sx={sidebarStyles.listItemButton}>
                         <ListItemIcon>
                             <QuestionMark />
                         </ListItemIcon>
                         <ListItemText primary="FAQs" />
                     </ListItemButton>
                 </Link>
-                <Link to="/logout">
-                    <ListItemButton>
+                <Link to="/logout" style={sidebarStyles.link}>
+                    <ListItemButton sx={sidebarStyles.listItemButton}>
                         <ListItemIcon>
                             <ExitToAppOutlined />
                         </ListItemIcon>
