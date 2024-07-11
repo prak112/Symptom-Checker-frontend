d901d139b9d74a89a497d3a114cdac3c7e685040
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-11 16:39:32 +0300

    chore: Move SymptomForm component from App

--- symptom-checker-frontend/src/pages/Home.jsx
+++ symptom-checker-frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
+import SymptomForm from "../components/SymptomForm"
+
 export default function Home(){
     return(
         <>
             <h3>Home page</h3>
-            <div>
-                Work in progress
-            </div>
+            <SymptomForm />
         </>
     )
 }
