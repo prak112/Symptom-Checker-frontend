0c073f0557e445b29e4634e5cf487db006045b58
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-17 00:57:06 +0300

    chore: Update font styling with @fontsource/roboto

--- symptom-checker-frontend/src/main.jsx
+++ symptom-checker-frontend/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 import {  BrowserRouter as Router } from 'react-router-dom'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
