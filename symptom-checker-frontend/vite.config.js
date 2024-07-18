8e90f284ac31d27e9895df83743a7c6f37c03a89
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-18 18:53:25 +0300

    chore: Add proxy config for /api/protected/symptoms endpoint

--- symptom-checker-frontend/vite.config.js
+++ symptom-checker-frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/protected/symptoms': {
+        target: 'http://localhost:3003',
+        changeOrigin: true
+      },
+    }
+  },
 })
