2a8ea5b12ee9a4a53f380e3abc69fcee89025b8f
PrakGov <55034062+prak112@users.noreply.github.com>
2024-07-19 17:21:05 +0300

    feat: Update diagnosis rendering and styling

--- symptom-checker-frontend/src/components/Diagnosis.jsx
+++ symptom-checker-frontend/src/components/Diagnosis.jsx
@@ -1,15 +1,36 @@
+import PropTypes from 'prop-types';
+
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
-//import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
+import LaunchIcon from '@mui/icons-material/Launch';  // for diagnosis info display
+
+import { 
+  Table, TableBody, TableCell, 
+  TableHead, TableRow, TableContainer, Paper 
+} from '@mui/material';   // for diagnosis info display
+import { Button } from '@mui/material';  // for Diagnosis info display
 import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-//import { createTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';  // for uniform font style
+//import Chip from '@mui/material/Chip';  // for Triage System design
+//import Stack from '@mui/material/Stack'; // for Diagnosis info display
 
-import PropTypes from 'prop-types';
-import { Button } from '@mui/material';
+//import { createTheme } from '@mui/material/styles';   // for Triage System design
 
-export default function Diagnosis({ label, score, url, title, detail }) {
+// check custom-colors MUI
+// const theme = createTheme({
+//   palette: {
+//     ochre: {
+//       main: '#E3D026',
+//       light: '#E9DB5D',
+//       dark: '#A29415',
+//       contrastText: '#242105',
+//     },
+//   },
+// });
+
+
+
+export default function Diagnosis({ data }) {
   // center and border style
   const cardStyle = {
     display: 'flex',
@@ -20,38 +41,59 @@ export default function Diagnosis({ label, score, url, title, detail }) {
     padding: '20px',
     border: '1px solid #ccc',
     borderRadius: '4px',    // rounded corners
-    width: '50%',        // container width
+    width: '70%',        // container width
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
   }
   
   Diagnosis.propTypes = {
-    label: PropTypes.string.isRequired,
-    score: PropTypes.number.isRequired,
-    url: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    detail: PropTypes.string.isRequired
+    data: PropTypes.object.isRequired
   };
 
-  // check custom-colors MUI
-  // const theme = createTheme({
-  //   palette: {
-  //     ochre: {
-  //       main: '#E3D026',
-  //       light: '#E9DB5D',
-  //       dark: '#A29415',
-  //       contrastText: '#242105',
-  //     },
-  //   },
-  // });
 
+  // TO DO : Validators for html Label, number Score
+  const isUrl = (value) => typeof value === 'string' && (value.startsWith('http://') || value.startsWith('https://')); 
+  
   return (
     <Card variant="outlined" sx={cardStyle}>
       <Box sx={{ p: 2 }}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center">
-          <Typography color='ActiveCaption' gutterBottom variant="h5" component="div">
-            {label}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
+      <TableContainer component={Paper} sx={{ maxHeight: '400px', overflow: 'auto' }} >
+      <Table sx={{ minWidth: 650 }} aria-label="diagnosis table">
+        <TableHead>
+          <TableRow>
+            {/* Assuming the first row of the table to be the categories from your data object */}
+            {Object.keys(data).map((category, index) => (
+              <TableCell key={index} align={index === 0 ? "inherit" : "right"}>{category}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/* Assuming each array in your data object has the same length */}
+          {data[Object.keys(data)[0]].map((_, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {Object.keys(data).map((category, colIndex) => (
+                <TableCell key={colIndex} align={colIndex === 0 ? "inherit" : "right"}>
+                  {isUrl(data[category][rowIndex]) ? (
+                    <Button 
+                      variant='outlined'
+                      endIcon={<LaunchIcon />}
+                      href={data[category][rowIndex]} 
+                      target="_blank" rel="noopener noreferrer">
+                      Read More
+                    </Button>
+                  ) : (
+                    <Typography variant="body2">{data[category][rowIndex]}</Typography>
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </TableContainer>
+      </Box>
+      <Divider />
+
+          {/* <Typography gutterBottom variant="h6" component="div">
             Diagnosis Score : {Math.round((+score * 100), 2)}%
           </Typography>
         </Stack>
@@ -66,8 +108,7 @@ export default function Diagnosis({ label, score, url, title, detail }) {
             Read More on ICD-11
           </Button>
         </Stack>
-      </Box>
-      <Divider />
+         */}
       
 {/*  TO DO: Understand and build Triage system logic  */}
 
