c1f72fd324ddf664eaddfb2e411be680ceb15781
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-07 22:02:02 +0300

    chore: Add style to center div block

--- symptom-checker-frontend/src/pages/Faqs.jsx
+++ symptom-checker-frontend/src/pages/Faqs.jsx
@@ -1,7 +1,13 @@
 export default function FAQ(){
+    const centeredDivStyle = {
+        width: "50%",
+        paddingTop: "20px",
+        margin: "0 auto"
+    }
+
     return(
         <>
-        <h3>FAQs</h3>
+        <h3 style={centeredDivStyle}>FAQs</h3>
         <div>
             ...coming soon...
         </div>
