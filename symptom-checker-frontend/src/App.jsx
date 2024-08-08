0af673c316d98eb8e92a20d1d484ab3433167b07
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-08 17:28:39 +0300

    feat: Update signup route to use ModalWrapper component

--- symptom-checker-frontend/src/App.jsx
+++ symptom-checker-frontend/src/App.jsx
@@ -9,6 +9,7 @@ import SignupModal from './components/modals/SignupModal'
 import FAQ from './pages/Faqs'
 
 import { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom'
 
 
@@ -27,13 +28,44 @@ function App() {
       <Sidebar open={drawerOpen} toggleDrawer={toggleDrawer}/> 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignupModal modalTitle="Sign Up!" />} /> {/*if user not authorized*/}
+        <Route path="/signup" element={<ModalWrapper />} />{/*if user not authorized*/}
         <Route path="/faqs" element={<FAQ />}/>
         <Route path="/logout" element={<Home />} />
       </Routes>
       <Footer />
     </div>  
   )
+}
+
+/**User authentication
+ * FEAT-setup auth services - DONE 
+ * FEAT-setup registration UI - DONE 
+ * DEBUG-signup : User registration fails without any error - FIXED
+ * FEAT-signup : setup navigation to modal - DONE
+ * FEAT-signup success : reroute to 'Home' - DONE
+ * FEAT-signup success : update Header with <Avatar> navigate to User Profile 
+ * FEAT-signup success : update Sidebar with Profile and Logout items
+ * FEAT-Update Sidebar with conditional render of 'Home', 'Signup', 'Login'-'Profile' 
+ * FEAT-setup login UI
+**/
+
+// Handler for Modals
+function ModalWrapper() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const returnHome = () => navigate('/')
+
+  return(
+    <>
+        <SignupModal 
+          modalTitle='Sign Up!' 
+          open={location.pathname==='/signup'}
+          handleClose={returnHome}
+        />
+    </>
+  )
+
 }
 
 export default App
