ab8e631a71ae1cc734a9b30d630d9e8a4dd9d072
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-08 17:25:08 +0300

    chore: Add proxy config for /public/auth endpoint

--- symptom-checker-frontend/vite.config.js
+++ symptom-checker-frontend/vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+const localhost = 'http://localhost:3003'
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api/protected/symptoms': {
-        target: 'http://localhost:3003',
+        target: localhost,
         changeOrigin: true
       },
+      '/public/auth': {
+        target: localhost,
+        changeOrigin: true
+      }
     }
   },
 })
