b029491342ebcc2c7e4479b9d989b138fde0a336
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-14 00:10:17 +0300

    feat: Add user avatar and context, Update PropTypes declaration

--- symptom-checker-frontend/src/components/Header.jsx
+++ symptom-checker-frontend/src/components/Header.jsx
@@ -1,16 +1,35 @@
-/*eslint-disable*/
-
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
+// react
+import PropTypes from 'prop-types'
+// context
+import { useContext } from 'react'
+import { UserContext } from '../contexts/UserContext';
+// materialUI
+import { AppBar, Toolbar, Typography, IconButton, Avatar } from "@mui/material";
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
 import MenuIcon from '@mui/icons-material/Menu';
+import { blue } from '@mui/material/colors';
+import { Link } from 'react-router-dom'
+// resources
 import Logo from '../assets/logo.svg'
 
 export default function Header({ toggleDrawer }){
-    const gradientStyle = { 
-        background: 'linear-gradient(45deg, #FFC107 30%, #FFEB3B 90%)' 
+    // get user info
+    const { user } = useContext(UserContext)
+    const appBarStyle = {
+        background: 'linear-gradient(45deg, #FFC107 30%, #FFEB3B 90%)',
+        padding:  '10px',
     }
+    const toolBarStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        paddingRight: '200px',
+    }
+
+    // username to render in <Avatar>
+
     return(
-        <AppBar position="relative" sx={gradientStyle} >
-            <Toolbar>
+        <AppBar position="relative" sx={appBarStyle} >
+            <Toolbar sx={toolBarStyle}>
                 <IconButton 
                     edge="start" 
                     color="inherit" 
@@ -22,10 +41,28 @@ export default function Header({ toggleDrawer }){
                 <img src={Logo} alt='Logo' 
                     width={75} height={75} 
                     style={{paddingRight: '50px', paddingLeft: '25px'}}/> 
-                <Typography variant="h5" align='justify'>
+                <Typography variant="h4" align="center" color="primary">
                     Symptom Checker
                 </Typography>
+                {user 
+                ? (
+                <Link to="/profile">
+                    <Avatar 
+                        sx={{ bgcolor: blue[200] }} 
+                        variant="rounded"
+                        alt={user}
+                    >
+                        <PermIdentityOutlinedIcon />
+                    </Avatar>
+                </Link>
+                )
+                : null}
             </Toolbar>
         </AppBar>
     )    
+}
+
+Header.propTypes = {
+    toggleDrawer: PropTypes.func.isRequired,
+    // registeredUser: PropTypes.string,
 }
