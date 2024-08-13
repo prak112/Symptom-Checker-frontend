73dc829ffd733d851fa77d31e60e0c3687060e2e
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-14 00:16:18 +0300

    feat: Add UserContext, Update Modal inner-element and PropTypes declaration

--- symptom-checker-frontend/src/components/modals/Login.jsx
+++ symptom-checker-frontend/src/components/modals/Login.jsx
@@ -2,15 +2,17 @@
 import { Box, Button, Modal, Typography, Divider } from '@mui/material'
 import { TextField, FormControlLabel, Checkbox, FormHelperText 
 } from '@mui/material'
-import { Login } from '@mui/icons-material'
+import { LoginOutlined } from '@mui/icons-material'
 import EnhancedEncryptionOutlined from '@mui/icons-material/EnhancedEncryptionOutlined';
 // react
 import PropTypes from 'prop-types'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 // resources
+import Copyright from '../Copyright';
 import authServices from '../../services/auth'
 import Logo from '../../assets/logo.svg'
+import { UserContext } from '../../contexts/UserContext'
 
 
 // styles
@@ -41,20 +43,17 @@ const centeredDivStyle = {
 }
 
 const modalStyle = {
-    background: 'linear-gradient(to top, #FFC107 10%, #FFEB3B 90%)' // matt-yellow
+    background: 'linear-gradient(to top, #FFC107 10%, #FFEB3B 90%)', // matt-yellow
     // background: 'linear-gradient(to bottom, #A3D1CC, #536B68)' // matt-green
 }
 
 export default function LoginModal({ open, handleClose }){
-    LoginModal.propTypes = {
-        open: PropTypes.bool.isRequired,
-        handleClose: PropTypes.func.isRequired,
-    }
     // setup states
     const [username, setUsername] = useState(null)
     const [password, setPassword] = useState(null)
     const [showPassword, setShowPassword] = useState(true)
     const navigate = useNavigate()
+    const { setUser } = useContext(UserContext)
 
     // Auth service handler
     const authenticateUser = async (event) => {
@@ -65,10 +64,10 @@ export default function LoginModal({ open, handleClose }){
                 username: username,
                 password: password
             }
-            const authenticatedUser = await authServices.authenticateUser(userInfoPayload)
+            const authorizedUser = await authServices.authenticateUser(userInfoPayload)
             // store username in sessionStorage for global access 
-            window.sessionStorage.setItem('authenticatedUser', JSON.stringify(authenticatedUser))
-            console.log('RESULT: ', authenticatedUser)
+            window.sessionStorage.setItem('authenticatedUser', authorizedUser.username)
+            setUser(authorizedUser.username)
             setUsername(null)
             setPassword(null)
             navigate('/')   // redirect to home
@@ -80,70 +79,77 @@ export default function LoginModal({ open, handleClose }){
     if(!open) return null;
 
     return(
-        <>
-            <div style={centeredDivStyle}>
-                <Button onClick={open}></Button>
-                <Modal
-                    sx={modalStyle}
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >            
+            <Modal
+                sx={modalStyle}
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="login-modal"
+            >            
+                <div style={centeredDivStyle}>
                     <Box sx={boxStyle}>
-                    <Typography variant="h4" align="center">
-                        Login
-                    </Typography>
-                    <div style={{display: 'flex', justifyContent:'center'}}>
-                        <img src={Logo} alt='Logo' 
-                            width={100} height={100} 
-                            style={{paddingRight: '50px', paddingLeft: '25px'}}
-                        />
-                        <Typography variant="h5" align="center">
-                            Symptom Checker and Triage System
-                        </Typography> 
-                    </div>
-                    <form onSubmit={authenticateUser} style={formStyle}>
-                        <TextField 
-                            id="username"
-                            type="text" 
-                            label="Username"
-                            placeholder="Username" 
-                            variant="outlined"
-                            required
-                            onChange={(e)=>setUsername(e.target.value)}
-                        />
-                        <TextField 
-                            id="password"
-                            type={showPassword ? "text" : "password"} 
-                            label="Password"
-                            placeholder="Password" 
-                            variant="outlined"
-                            required
-                            onChange={(e)=>setPassword(e.target.value)}
-                        />
-                        <FormControlLabel
-                            control={
-                                <Checkbox 
-                                    checked={showPassword}
-                                    onChange={(e) => setShowPassword(e.target.checked)}
-                                />
-                            }
-                            label="Show Password"
-                        />
-                        <FormHelperText id="helper-text">
-                            <EnhancedEncryptionOutlined />
-                            All your data and symptom data is <em>ALWAYS end-to-end encrypted</em>,
-                            which means only <em>YOU</em> see the real data, others see encrypted gibberish.
-                        </FormHelperText>
-                        <Button type="submit" endIcon={<Login />} variant="outlined" color="secondary">
-                            Login!
-                        </Button>
-                    </form>
-                    <Divider />
-                </Box>
-                </Modal>
-            </div>
-        </>
+                        <Typography variant="h4" align="center">
+                            Login
+                        </Typography>
+                        <div style={{display: 'flex', justifyContent:'center'}}>
+                            <img src={Logo} alt='Logo' 
+                                width={100} height={100} 
+                                style={{paddingRight: '50px', paddingLeft: '25px'}}
+                            />
+                            <Typography variant="h5" align="center">
+                                Symptom Checker and Triage System
+                            </Typography> 
+                        </div>
+                        <form onSubmit={authenticateUser} style={formStyle}>
+                            <TextField 
+                                id="username"
+                                type="text" 
+                                label="Username"
+                                placeholder="Username" 
+                                variant="outlined"
+                                required
+                                onChange={(e)=>setUsername(e.target.value)}
+                            />
+                            <TextField 
+                                id="password"
+                                type={showPassword ? "text" : "password"} 
+                                label="Password"
+                                placeholder="Password" 
+                                variant="outlined"
+                                required
+                                onChange={(e)=>setPassword(e.target.value)}
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox 
+                                        checked={showPassword}
+                                        onChange={(e) => setShowPassword(e.target.checked)}
+                                    />
+                                }
+                                label="Show Password"
+                            />
+                            <FormHelperText id="helper-text">
+                                <EnhancedEncryptionOutlined />
+                                All your data and symptom data is <em>ALWAYS end-to-end encrypted</em>,
+                                which means only <em>YOU</em> see the real data, others see encrypted gibberish.
+                            </FormHelperText>
+                            <Button 
+                                type="submit" 
+                                endIcon={<LoginOutlined /> } 
+                                variant="outlined" 
+                                color="secondary"
+                            >
+                                Login!
+                            </Button>
+                        </form>
+                        <Divider />
+                        <Copyright />
+                    </Box>
+                </div>
+            </Modal>
     )
+}
+
+LoginModal.propTypes = {
+    open: PropTypes.bool.isRequired,
+    handleClose: PropTypes.func.isRequired,
 }
