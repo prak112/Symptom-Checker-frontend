fc582a8c7a6da10c1eb78f3354b78d85d6505fe9
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-15 15:57:44 +0300

    docs: Add documentation

--- symptom-checker-frontend/src/contexts/UserContext.jsx
+++ symptom-checker-frontend/src/contexts/UserContext.jsx
@@ -1,9 +1,21 @@
 import { createContext, useState, useEffect } from "react";
 import PropTypes from 'prop-types'
 
-// Context component for App component
+/**
+ * UserContext
+ * @type {React.Context}
+ * @desc A context object for managing user data.
+ */
 export const UserContext = createContext();
 
+/**
+ * UserProvider component.
+ * 
+ * @component
+ * @param {Object} props - The component props.
+ * @param {ReactNode} props.children - The child components.
+ * @returns {JSX.Element} The rendered UserProvider component.
+ */
 export function UserProvider({ children }){
     const [user, setUser] = useState(null)
     // get and store user information
