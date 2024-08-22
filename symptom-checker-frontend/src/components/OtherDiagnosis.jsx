bcea3f4c0922eb0adc6a2ea1bc217d33278c3170
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-22 20:34:35 +0300

    refactor: Handle Diagnosis data for user-friendly display

---
+++ symptom-checker-frontend/src/components/OtherDiagnosis.jsx
@@ -0,0 +1,55 @@
+// react
+import PropTypes from 'prop-types'
+import { useState } from 'react' 
+// material UI
+import { Box, Button, Chip } from '@mui/material'
+import { ExpandMoreOutlined } from '@mui/icons-material'
+
+export default function OtherDiagnosis({ resultSet, type }){
+    // setup results limitation 
+    const [displayLimit, setDisplayLimit] = useState(5)
+    const showMore = () => {
+        setDisplayLimit(prevLimit => prevLimit + 5)
+    }
+    const displayedResults = resultSet.label.slice(0, displayLimit)
+
+    return(
+        <Box display="flex" flexDirection="column" gap={1}>
+            {displayedResults.map((label, index) => (
+            <Chip 
+                key={index}
+                variant="outlined"
+                color={type === 'included' ? "secondary" : "info"}
+                label={
+                    <span 
+                        dangerouslySetInnerHTML={{ __html: `${label} : ${resultSet.score[index]}%` }} 
+                    />
+                }
+                component="a"
+                href={resultSet.url[index]}
+                target="_blank" 
+                rel="noopener noreferrer"
+                clickable
+            />
+            ))}
+            {displayLimit < resultSet.label.length && (
+                <Button
+                    onClick={showMore}
+                    variant="filled"
+                    size="small"
+                    color="info"
+                    endIcon={<ExpandMoreOutlined />}
+                />
+            )}
+        </Box>
+    )
+}
+
+OtherDiagnosis.propTypes = {
+    resultSet: PropTypes.shape({
+        label: PropTypes.array.isRequired,
+        score: PropTypes.array.isRequired,
+        url: PropTypes.array.isRequired,
+    }).isRequired,
+    type: PropTypes.string.isRequired,
+}
