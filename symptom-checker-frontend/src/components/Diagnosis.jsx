923027b8a392f3129deb4f23809280d670c889c6
PrakGov <55034062+prak112@users.noreply.github.com>
2024-08-22 20:32:19 +0300

    refactor: Simplify render logic, Split Diagnosis data to sub-components

--- symptom-checker-frontend/src/components/Diagnosis.jsx
+++ symptom-checker-frontend/src/components/Diagnosis.jsx
@@ -1,13 +1,12 @@
 // react
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-//material UI
-import { ArrowBack, Launch } from '@mui/icons-material'  
-import { 
-  Table, TableBody, TableCell, 
-  TableHead, TableRow, TableContainer, Paper 
-} from '@mui/material'
-import { Card, Box, Button, Divider, Typography } from '@mui/material'
+// material UI
+import { ArrowBack } from '@mui/icons-material'  
+import { Card, Box, Chip, Button, Divider, Typography } from '@mui/material'
+// components
+import TopDiagnosis from './TopDiagnosis'
+import OtherDiagnosis from './OtherDiagnosis'
 
 // styles
 const cardStyle = {
@@ -15,11 +14,11 @@ const cardStyle = {
   flexDirection: 'column',
   justifyContent: 'center',
   margin: 'auto',
-  marginBottom: '10vh',  // vertical adjustment
+  marginBottom: '5vh',  // vertical adjustment
   padding: '20px',
   border: '1px solid #ccc',
   borderRadius: '4px',    // rounded corners
-  width: '80%',        // container width
+  width: '90%',        // container width
   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
 }
 
@@ -33,104 +32,61 @@ const cardStyle = {
  * @returns {JSX.Element} The rendered Diagnosis component.
  */
 export default function Diagnosis({ data, handleReturn }) {
-  // data validators
-  const isUrl = (value) => typeof value === 'string' && (value.startsWith('http://') || value.startsWith('https://')); 
-  const isScore = (value) => {
-    const scoreRegex = /-?\d+(\.\d+)?/;
-    return scoreRegex.test(value);
-  }
-  const isHtml = (value) => {
-    return typeof value === 'string' && value.includes('<')
-  }
+  const symptom = data.symptom
+  const topResult = data.topResult
+  const includedResults = data.includedResults
+  const excludedResults = data.excludedResults
 
-  /** REFACTOR <Diagnosis />
-   * <Diagnosis /> must be split into 2 sub-components to accomodate :
-    * 'topResult' = <TopDiagnosis />
-    * 'includedResults', 'excludedResults' = <OtherDiagnosis />
-  * Render <TopDiagnosis /> with Label, Detail, Url and Score
-  * Render <OtherDiagnosis /> as follows :
-    * <Chip /> for results from 'includedResults' inside <Box /> with
-      * Heading - 'Other Suggested Results'
-      * Content - `${Label} : {Score}%`
-      * Hyperlink with browserUrl
-    *
-    * <Chip /> for results from 'excludedResults' inside <Box /> with
-      * Heading - 'Excluded Results'
-      * Content - `${Label} : {Score}%`
-      * Hyperlink with browserUrl
-    *
-    * Initially 5 results for each Result Set, 
-      * when user clicks 'Show All {results.total}', render all.      
-  **/
-
-  return (
+  return(
     <Card variant="outlined" sx={cardStyle}>
-      <Box sx={{ p: 2 }}>
-        <TableContainer component={Paper} sx={{ maxHeight: '400px', overflow: 'auto' }} >
-          <Table sx={{ minWidth: 650 }} aria-label="diagnosis table">
-            <TableHead>
-              <TableRow>
-                {/* Assuming the first row of the table to be the categories from data object */}
-                {Object.keys(data).map((category, index) => (
-                  <TableCell 
-                    key={index} 
-                    align={index === 0 ? "inherit" : "right"}
-                    sx={{ position: 'sticky', top: 0, backgroundColor: 'beige', zIndex: 1 }}
-                    >
-                    {category}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {/* Assuming each array in your data object has the same length */}
-              {data[Object.keys(data)[0]].map((_, rowIndex) => (
-              <TableRow key={rowIndex}>
-                {Object.keys(data).map((category, colIndex) => (
-                  <TableCell key={colIndex} align={colIndex === 0 ? "inherit" : "right"}>
-                    {isUrl(data[category][rowIndex]) ? (
-                      <Button 
-                        variant='outlined'
-                        endIcon={<Launch />}
-                        href={data[category][rowIndex]} 
-                        target="_blank" rel="noopener noreferrer">
-                        Read More
-                      </Button>
-                    ) : isScore(data[category][rowIndex]) ? (
-                      <Typography variant="body2" color="primary">
-                        {Math.round((+(data[category][rowIndex]) * 100), 2)}%
-                      </Typography>
-                    ) : isHtml(data[category][rowIndex]) ? (
-                      <Typography variant="body2" dangerouslySetInnerHTML={{ __html: data[category][rowIndex] }} />
-                    ) : (
-                      <Typography variant="body2">{data[category][rowIndex]}</Typography>
-                    )}
-                  </TableCell>
-                ))}
-              </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+      <Typography gutterBottom variant="h4" component="div">
+        {symptom}
+      </Typography>
+      <Box>
+        <TopDiagnosis 
+          label={topResult.label}
+          score={topResult.score}
+          detail={topResult.detail}
+          url={topResult.url}
+        />
+        <Divider>
+          <Chip label="Suggested Diagnosis" color="secondary" size="small" />
+        </Divider>
+        <OtherDiagnosis 
+          resultSet={includedResults} 
+          type="included"  
+        />
+        <Divider>
+          <Chip label="Excluded Diagnosis" color="info" size="small" />
+        </Divider>
+        <OtherDiagnosis 
+          resultSet={excludedResults} 
+          type="excluded" 
+        />
+        <Divider />
         <Button 
           component={Link} to="/" 
           endIcon={<ArrowBack />} 
-          variant='outlined'
-          onClick={handleReturn}
+          variant='outlined' 
+          onClick={handleReturn} 
           sx={{ marginTop: '20px' }}
         >
           Check again ?
         </Button>
         <Typography sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-          Total Results : n+1
+          Total Results : {includedResults.label.length + excludedResults.label.length}
         </Typography>
       </Box>
-      <Divider />
     </Card>
   )
 }
 
 Diagnosis.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    symptom: PropTypes.string.isRequired,
+    topResult: PropTypes.object.isRequired,
+    includedResults: PropTypes.object.isRequired,
+    excludedResults: PropTypes.object.isRequired,
+}).isRequired,
   handleReturn: PropTypes.func.isRequired,
 };
