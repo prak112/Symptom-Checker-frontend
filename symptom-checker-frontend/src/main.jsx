e0b3d9b807c966d88518ec484ca2bc16ee88bd9a
PrakGov <55034062+prak112@users.noreply.github.com>
2024-09-12 18:27:31 +0300

    refactor: Add context providers and component

--- symptom-checker-frontend/src/main.jsx
+++ symptom-checker-frontend/src/main.jsx
@@ -7,12 +7,23 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { AuthenticationProvider } from './contexts/AuthenticationContext.jsx'
+import { UserProvider } from './contexts/UserContext.jsx'
+import { AlertProvider } from './contexts/AlertContext.jsx'
+import Footer from './components/Footer.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <React.StrictMode>
-      <App />
+      <UserProvider>
+        <AlertProvider>
+          <AuthenticationProvider>
+            <App />
+            <Footer />
+          </AuthenticationProvider>
+        </AlertProvider>
+      </UserProvider>
     </React.StrictMode>
   </Router>
 )
