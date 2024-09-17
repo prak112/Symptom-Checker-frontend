d177059bbab50e121d7f6a5976ac75a471bfba98
PrakGov <55034062+prak112@users.noreply.github.com>
2024-09-17 22:09:35 +0300

    feat: Generate and render User History

--- symptom-checker-frontend/src/pages/UserHistory.jsx
+++ symptom-checker-frontend/src/pages/UserHistory.jsx
@@ -1,40 +1,130 @@
-// react
-// import PropTypes from 'prop-types'
-import { useEffect, useState, useCallback } from 'react'
 // material UI
-import { Typography } from '@mui/material'
+import { KeyboardReturnOutlined } from '@mui/icons-material';
+import { Box, FormHelperText, Typography, Button } from '@mui/material'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+  } from '@mui/material'
 // service
 import symptomsService from '../services/symptoms'
+// context
+import { SubmitFormContext } from '../contexts/SubmitFormContext'
+import { useState, useContext, useCallback, useEffect } from 'react'
+import { AuthenticationContext } from '../contexts/AuthenticationContext'
+import { useNavigate } from 'react-router-dom';
 
+// styles
+const centeredDivStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "75%",
+    paddingTop: "20px",
+    margin: "0 auto"
+}
 
-export default function UserProfile(){
-    const [searchHistory, setSearchHistory] = useState({})
+export default function UserHistory() {
+    const { submitted } = useContext(SubmitFormContext)
+    const { isAuthenticated } = useContext(AuthenticationContext)
+    const [data, setData] = useState([])
+    const navigate = useNavigate()
 
-    // styles
-    const centeredDivStyle = {
-        width: "50%",
-        paddingTop: "20px",
-        margin: "0 auto"
-    }
+    // handle return to Home
+    const returnHome = () => navigate('/')
 
-    // call symptoms service function
-    const getUserDiagnosis = useCallback(() => {
-        async() => {
-            const diagnosisTillNow = await symptomsService.getUserHistory()
-            setSearchHistory(diagnosisTillNow)
-        }
+    // using context and monitoring for changes in submitted state
+    const getUserDiagnosis = useCallback(async () => {
+        const diagnosisTillNow = await symptomsService.getUserHistory()
+        setData(diagnosisTillNow)
     }, [])
 
-    // monitor and refresh user history
     useEffect(() => {
-        getUserDiagnosis()
-    }, [searchHistory, getUserDiagnosis])
+        if(isAuthenticated){ 
+            getUserDiagnosis() 
+        }
+    }, [isAuthenticated, submitted, getUserDiagnosis])
 
+    // verify empty history data
+    if (!data || data.length === 0) {
+        return (
+            <Box sx={centeredDivStyle}>
+                <Typography variant="body1">
+                    Nothing from your past, yet! Go check some symptoms.
+                </Typography>
+            </Box>
+        )
+    }
+
+    // Extract and modify necessary data
+    const rows = data.diagnosis.flatMap(diagnosisItem => {
+        return diagnosisItem.diagnosis.map((record) => ({
+            symptom: record.symptom,
+            analysis: record.analysis.toUpperCase(),
+            diagnosedAt: new Date(record.diagnosedAt).toString(),
+            topResult: record.topResult || {},
+        }))
+    })
 
     return(
-        <div style={centeredDivStyle}>
-        <Typography variant="h5">Diagnosis History</Typography>
-            <Typography variant="body2">...coming soon...</Typography>
-        </div>
+        <Box sx={centeredDivStyle}>
+            <Typography variant="h5">Diagnosis History</Typography>
+            <FormHelperText variant="outlined" focused>
+                <strong>*History only shows the top diagnosis for each symptom/search</strong>
+            </FormHelperText>
+            <Button
+                display="flex"
+                direction="row"
+                type="submit" 
+                endIcon={<KeyboardReturnOutlined />} 
+                variant="outlined" 
+                color="secondary"
+                onClick={returnHome}
+            >
+                Back
+            </Button>
+            <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                <TableRow>
+                    <TableCell sx={{ width: '15%' }}>Symptom</TableCell>
+                    <TableCell sx={{ width: '10%' }}>Analysis</TableCell>
+                    <TableCell sx={{ width: '10%' }}>Diagnosed At</TableCell>
+                    <TableCell sx={{ width: '10%' }}>Diagnosis</TableCell>
+                    <TableCell sx={{ width: '10%' }}>Relevance</TableCell>
+                    <TableCell sx={{ width: '40%' }}>Details</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {rows.map((row, index) => (
+                    <TableRow key={index}>
+                        <TableCell sx={{ width: '15%' }}>{row.symptom}</TableCell>
+                        <TableCell sx={{ width: '10%' }}>{row.analysis}</TableCell>
+                        <TableCell sx={{ width: '10%' }}>{row.diagnosedAt}</TableCell>
+                        <TableCell sx={{ width: '10%' }}>{row.topResult.title}</TableCell>
+                        <TableCell sx={{ width: '10%' }}>{row.topResult.score}%</TableCell>
+                        <TableCell sx={{ width: '40%' }}>
+                            {row.topResult.detail}<br/>
+                            {row.topResult.url 
+                                ? ( <a 
+                                        href={row.topResult.url} 
+                                        target="_blank" 
+                                        rel="noopener noreferrer"
+                                    >
+                                        Read More
+                                    </a>
+                                ) : 'No Data Available'
+                            }
+                    </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+        </Box>
     )
 }
