b23c7b4198ad19d6352809decd1afb870856269a
PrakGov <55034062+prak112@users.noreply.github.com>
2024-09-18 20:57:16 +0300

    feat: Add basic user info and functionality

--- symptom-checker-frontend/src/pages/UserProfile.jsx
+++ symptom-checker-frontend/src/pages/UserProfile.jsx
@@ -1,22 +1,41 @@
 // context
+import { Typography, Button } from '@mui/material';
 import { UserContext } from '../contexts/UserContext';
 import { useContext } from 'react';
+import { KeyboardReturnRounded } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 export default function UserProfile() {
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate()
+
     // styles
     const centeredDivStyle = {
         display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
         width: "50%",
-        paddingTop: "20px",
-        margin: "0 auto"
+        paddingTop: "100px",
+        margin: "auto"
     }
-    const { user } = useContext(UserContext);
 
     return (
         <div style={centeredDivStyle}>
-            <p>
+            <Typography variant="h5">User Information</Typography>
+            <Typography variant="subtitle1">
                 Username : {user.toUpperCase()}
-            </p>
+            </Typography>
+            <Typography variant="subtitle2">
+                Using since : {new Date(user.registrationTime).toLocaleString()}
+            </Typography>
+            <Button 
+                color="info"
+                variant="outlined" 
+                endIcon={<KeyboardReturnRounded />}
+                onClick={() => navigate('/')}
+            >
+                Back
+            </Button>
         </div>
     )
 }
