4ca93f3f09024819344e0d356e298e57a627a9f3
PrakGov <55034062+prak112@users.noreply.github.com>
2024-09-30 20:30:33 +0300

    refactor: Adjust menubar layout and styles

--- symptom-checker-frontend/src/components/Header.jsx
+++ symptom-checker-frontend/src/components/Header.jsx
@@ -1,14 +1,13 @@
 // react
 import PropTypes from 'prop-types'
-// context
+import { Link } from 'react-router-dom'
 import { useContext } from 'react'
+// context
 import { UserContext } from '../contexts/UserContext';
 // materialUI
-import { AppBar, Toolbar, Typography, IconButton, Avatar } from "@mui/material";
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
-import MenuIcon from '@mui/icons-material/Menu';
-import { blueGrey } from '@mui/material/colors';
-import { Link } from 'react-router-dom'
+import { Box, AppBar, Toolbar, Typography, IconButton, Avatar } from "@mui/material";
+import { Menu, PermIdentityOutlined } from '@mui/icons-material';
+import { deepPurple } from '@mui/material/colors';
 // resources
 import Logo from '../assets/logo.svg'
 
@@ -16,25 +15,34 @@ export default function Header({ toggleDrawer }){
     // get user info
     const { user } = useContext(UserContext)
     const appBarStyle = {
-        display: "flex",
-        justifyContent: "space-between",
-        background: "linear-gradient(45deg, #FFC107 30%, #FFEB3B 90%)",
+        display: 'flex',
+        margin: '0px auto',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        background: 'linear-gradient(45deg, #FFC107 30%, #FFEB3B 90%)',
+        paddingLeft: '25px',
+        paddingRight: '50px', 
         py: 2,  // padding top, bottom
-        mr: 2,  // margin right
     }
     const toolBarStyle = {
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        flexDirection: "row",
+        marginLeft: '50px',
+        marginRight: '78px',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'row',
     }
     const avatarStyle = {
-        bgcolor: blueGrey['A400'],
+        marginLeft: '50px',
+        marginRight: '50px',
+        bgcolor: deepPurple['300'],
     }
     const imgStyle = {
         display: 'block',
-        paddingRight: '50px', 
+        marginLeft: '50px',
+        marginRight: '50px',
         paddingLeft: '25px',
+        paddingRight: '50px', 
     }
 
     return(
@@ -45,8 +53,9 @@ export default function Header({ toggleDrawer }){
                     color="inherit" 
                     aria-label="menu" 
                     onClick={toggleDrawer}
+                    size="small"
                 >
-                    <MenuIcon fontSize='large' />
+                    <Menu fontSize="large" />
                 </IconButton>
                 <Link to="/">
                     <img src={Logo} alt='Logo' 
@@ -59,15 +68,21 @@ export default function Header({ toggleDrawer }){
                 </Typography> 
                 {user 
                 ? (
-                <Link to="/profile">
-                    <Avatar 
-                        sx={avatarStyle} 
-                        variant="rounded"
-                        alt={user.username}
-                    >
-                        <PermIdentityOutlinedIcon />
-                    </Avatar>
-                </Link>
+                <Box
+                    display="flex" 
+                    flexDirection="column" 
+                    alignItems="flex-end"
+                >
+                    <Link to="/profile">
+                        <Avatar 
+                            sx={avatarStyle} 
+                            variant="circular"
+                            alt={user.username}
+                        >
+                            <PermIdentityOutlined fontSize="large" />
+                        </Avatar>
+                    </Link>
+                </Box>
                 )
                 : null}
             </Toolbar>
@@ -77,5 +92,4 @@ export default function Header({ toggleDrawer }){
 
 Header.propTypes = {
     toggleDrawer: PropTypes.func.isRequired,
-    // registeredUser: PropTypes.string,
 }
