af123e1fa774f0933788d084015adeb6823e8d3c
PrakGov <55034062+prak112@users.noreply.github.com>
2024-09-30 20:56:58 +0300

    debug: To handle registrationTime render error

--- symptom-checker-frontend/src/pages/UserProfile.jsx
+++ symptom-checker-frontend/src/pages/UserProfile.jsx
@@ -1,9 +1,11 @@
-// context
-import { Typography, Button } from '@mui/material';
-import { UserContext } from '../contexts/UserContext';
+// react
 import { useContext } from 'react';
-import { KeyboardReturnRounded } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
+// material UI
+import { Typography, Button } from '@mui/material';
+import { KeyboardReturnRounded } from '@mui/icons-material';
+
 
 export default function UserProfile() {
     const { user } = useContext(UserContext);
@@ -19,14 +21,19 @@ export default function UserProfile() {
         margin: "auto"
     }
 
+    // Check if user is null or undefined
+    if (!user) {
+        return <Typography variant="h5">Loading...</Typography>;
+    }
+
     return (
         <div style={centeredDivStyle}>
             <Typography variant="h5">User Information</Typography>
             <Typography variant="subtitle1">
-                Username : {user.toUpperCase()}
+                Username : {user ? user.toUpperCase() : 'N/A'}
             </Typography>
             <Typography variant="subtitle2">
-                Using since : {new Date(user.registrationTime).toLocaleString()}
+                Using since : {user.registrationTime ? user.registrationTime : 'N/A'}
             </Typography>
             <Button 
                 color="info"
