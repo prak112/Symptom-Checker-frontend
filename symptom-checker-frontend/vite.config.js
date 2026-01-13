6dcefa31f5d5a7af3786d77fbcc43a0fae1f301c
PrakGov <55034062+prak112@users.noreply.github.com>
2026-01-13 19:35:41 +0200

    deploy: Add build config for deployment

--- symptom-checker-frontend/vite.config.js
+++ symptom-checker-frontend/vite.config.js
@@ -3,18 +3,25 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 const localhost = 'http://localhost:3003'
-export default defineConfig({
+export default defineConfig(({
   plugins: [react()],
   server: {
     proxy: {
+      //symptoms.js service
       '/api/protected': {
         target: localhost,
         changeOrigin: true
       },
-      '/public/auth': {
+      //auth.js service
+      '/public/auth': { 
         target: localhost,
         changeOrigin: true
       }
-    }
+    } 
   },
-})
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
+}));
